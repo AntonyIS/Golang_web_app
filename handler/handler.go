f1c1d39d31da9a44ac85d3233e3876745952706e
AntonyIS <antony@ariyafinergy.com>
2021-08-11 08:13:52 +0300

    Added variables into html page

--- handler/handler.go
+++ handler/handler.go
@@ -1,14 +1,13 @@
 package handler
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 )
 
 type PageContent struct {
-	Title   string
-	Content string
+	Title string
+	About string
 }
 
 func render_templete(res http.ResponseWriter, html_page string, page *PageContent) {
@@ -18,12 +17,16 @@ func render_templete(res http.ResponseWriter, html_page string, page *PageConten
 	html_page = "./templates/" + html_page
 	t, err := template.ParseFiles(html_page)
 
-	// Check if theres an error retrieving html page
+	/*
+		Check if theres an error retrieving html page
+	*/
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	// Check if there is an error rendering html page
+	/*
+		Check if there is an error rendering html page
+	*/
 	err = t.Execute(res, page)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
@@ -37,10 +40,10 @@ func index(res http.ResponseWriter, req *http.Request) {
 		Returns html page with content
 	*/
 	Page := &PageContent{
-		Title:   "Go Web App",
-		Content: "Welcome to my first Go web app",
+		Title: "Go Web App",
+		About: "One of Golang’s biggest advantages is that it offers the clarity and ease-of-use that other languages lack. Golang’s advantages make it easy for new programmers to quickly understand the language and for seasoned veterans to easily read each other’s code.",
 	}
-	fmt.Printf("HELLO %v", Page.Title)
+
 	render_templete(res, "index.html", Page)
 }
 
